8f996b237c0d62b3294e24dc3e63af19c22e0590
hrpc <hrpc@hrpcdeMacBook-Pro.local>
2017-03-06 09:54:18 +0800

    postcss

--- vue2new/postcss.config.js
+++ vue2new/postcss.config.js
@@ -1,13 +1,13 @@
-var pxtorem = require('postcss-pxtorem');
-var options = {
-    rootValue: 75,
-    unitPrecision: 5,
-    propList: ['*','!border'],
-    selectorBlackList: [],
-    replace: true,
-    mediaQuery: false,
-    minPixelValue: 2
-};
+//var pxtorem = require('postcss-pxtorem');
+//var options = {
+//  rootValue: 75,
+//  unitPrecision: 5,
+//  propList: ['*','!border'],
+//  selectorBlackList: [],
+//  replace: true,
+//  mediaQuery: false,
+//  minPixelValue: 2
+//};
 module.exports = {
 	plugins:[
 		require('autoprefixer'),
